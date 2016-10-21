9f8f24e17498b8b44308de72d5951ebdf1009b12
Jobayer Arman <carbonjha@gmail.com>
2016-10-22 05:29:13 +0600

    intro section
    + hamburger menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-	console.log('hello');
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+var target    = $("#header-top, .sidebar-wrapper");
+
+// On click
+hamburger.addEventListener("click", function() {
+    // Toggle class
+    hamburger.classList.toggle("is-active");
+    target.toggleClass("active");
 });
+
+var headerPad = (300 - ((window.innerWidth - 1170) / 2)) + 15;
+var sheet = document.createElement('style');
+sheet.innerHTML = "#header-top.active {padding-right: " + headerPad + "px;}";
+document.body.appendChild(sheet);
