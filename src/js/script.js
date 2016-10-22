eaf5004e75500e9aa9e7bfd3077e09e7cc5de9bf
Jobayer Arman <carbonjha@gmail.com>
2016-10-22 14:29:48 +0600

    update 0.1.2
    + more sections
    + fixed top nav
    + sidebar menu
    + browserSync inject CSS fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 // Look for .hamburger
 var hamburger = document.querySelector(".hamburger");
-var target    = $("#header-top, .sidebar-wrapper");
+var target    = $(".fixed-top, .sidebar-wrapper");
 
 // On click
 hamburger.addEventListener("click", function() {
@@ -11,5 +11,21 @@ hamburger.addEventListener("click", function() {
 
 var headerPad = (300 - ((window.innerWidth - 1170) / 2)) + 15;
 var sheet = document.createElement('style');
-sheet.innerHTML = "#header-top.active {padding-right: " + headerPad + "px;}";
+sheet.innerHTML = ".fixed-top.active {padding-right: " + headerPad + "px;}";
 document.body.appendChild(sheet);
+
+var anchor = $('a[href^="#"]');
+anchor.on('click',function (e) {
+  e.preventDefault();
+
+  var target = this.hash;
+  var $target = $(target);
+
+  $('html, body').stop().animate({
+    'scrollTop': $target.offset().top - 30
+  }, 900, 'swing');
+});
+
+var hero = $('#intro');
+var height = window.innerHeight - 60;
+hero.css({"min-height" : height + "px"});
