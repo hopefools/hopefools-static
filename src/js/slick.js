f0fe0940a9581f595585931e3ad9880ac0992e98
Jobayer Arman <carbonjha@gmail.com>
2016-10-28 04:24:59 +0600

    Fixed issue #1

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,35 +1,3 @@
-// Look for .hamburger
-var hamburger = document.querySelector(".hamburger");
-var target    = $(".fixed-top, .sidebar-wrapper");
-
-// On click
-hamburger.addEventListener("click", function() {
-    // Toggle class
-    hamburger.classList.toggle("is-active");
-    target.toggleClass("active");
-});
-
-var headerPad = (300 - ((window.innerWidth - 1170) / 2)) + 15;
-var sheet = document.createElement('style');
-sheet.innerHTML = ".fixed-top.active {padding-right: " + headerPad + "px;}";
-document.body.appendChild(sheet);
-
-var anchor = $('a[href^="#"]');
-anchor.on('click',function (e) {
-  e.preventDefault();
-
-  var target = this.hash;
-  var $target = $(target);
-
-  $('html, body').stop().animate({
-    'scrollTop': $target.offset().top - 30
-  }, 900, 'swing');
-});
-
-var hero = $('#intro');
-var height = window.innerHeight - 60;
-hero.css({"min-height" : height + "px"});
-
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
